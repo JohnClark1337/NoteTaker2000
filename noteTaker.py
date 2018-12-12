8c948c02c354b5346a3ce3f4c94e86b8906ce4d4
JohnClark1337 <jdsco37@gmail.com>
2018-12-12 16:54:12 -0500

    Add files via upload

--- noteTaker.py
+++ noteTaker.py
@@ -1,5 +1,19 @@
 import tkinter
 
+
+def saveFile():
+    print(entryBox.get(1.0))
+    try:
+        s = open("note1.txt", "w")
+        data = entryBox.get(1.0)
+        s.write(data)
+        s.close()
+    except:
+        print("Unable to save data.")
+
+
+
+
 mainWindow = tkinter.Tk()
 mainWindow.title("Note Taking Application")
 mainWindow.geometry("800x450")
@@ -11,30 +25,40 @@ sButtonHeight=2
 sButtonWidth = 25
 mainWindow.columnconfigure(0, weight=5)
 mainWindow.columnconfigure(1, weight=5)
+mainWindow.columnconfigure(2, weight=5)
 mainWindow.rowconfigure(0, weight=5)
 mainWindow.rowconfigure(1, weight=5)
 mainWindow.rowconfigure(2, weight=5)
 mainWindow.rowconfigure(3, weight=5)
 mainWindow.rowconfigure(4, weight=5)
+
 labelFrame = tkinter.Frame(mainWindow)
 textFrame = tkinter.Frame(mainWindow)
 buttonFrame = tkinter.Frame(mainWindow)
-entryBox = tkinter.Text(textFrame)
+
+scrollbar = tkinter.Scrollbar(textFrame)
+entryBox = tkinter.Text(textFrame, yscrollcommand=scrollbar.set)
 lblMain = tkinter.Label(labelFrame, text="Note Taker 2000", font=("Helvetica", 32))
 btnExit = tkinter.Button(buttonFrame, text="Exit", command=mainWindow.destroy, height=sButtonHeight, width=sButtonWidth)
-btnSave = tkinter.Button(buttonFrame, text="Save", height=sButtonHeight, width=sButtonWidth)
+btnSave = tkinter.Button(buttonFrame, text="Save", height=sButtonHeight, width=sButtonWidth, command=saveFile())
 btnLoad = tkinter.Button(buttonFrame, text="Load", height=sButtonHeight, width=sButtonWidth)
-labelFrame.grid(row=0, column=0, columnspan=2, sticky='n')
-textFrame.grid(row=1, column=0, rowspan=4, sticky='news')
-buttonFrame.grid(row=1, column=1, columnspan=2, sticky='e')
-lblMain.grid(sticky='news')
-entryBox.grid(sticky='news')
-btnLoad.grid(sticky='news')
-btnSave.grid(sticky='news')
-btnExit.grid(sticky='news')
+
+labelFrame.pack(side='top')
+textFrame.pack(side='left')
+buttonFrame.pack(side='right', fill='both', expand=True)
+
+scrollbar.pack(side='right', fill='both')
+lblMain.pack()
+entryBox.pack()
+scrollbar.config(command=entryBox.yview)
+btnLoad.pack(side='top')
+btnSave.pack(side='top')
+btnExit.pack(side='bottom')
 
 
 
 mainWindow.mainloop()
 
 
+
+    
